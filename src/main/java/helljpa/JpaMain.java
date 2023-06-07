b507169962d9a60b23d8fefb241e46ce18bb594e
yshong1998 <yshong1998@gmail.com>
2023-06-07 16:27:20 +0900

    Hello JPA - 애플리케이션 개발

--- src/main/java/helljpa/JpaMain.java
+++ src/main/java/helljpa/JpaMain.java
@@ -1,15 +1,31 @@
 package helljpa;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
         EntityManager em = emf.createEntityManager();
-        //code
-        em.close();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+            List<Member> result = em.createQuery("select m from  Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         emf.close();
 
 
